1d37f452d8bad67e5c16f60252e6c7f11b05d1fb
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-04-15 09:10:34 -0400

    Update understanding_gms_growth.sql

--- understanding_gms_growth.sql
+++ understanding_gms_growth.sql
@@ -47,6 +47,84 @@ count(distinct listing_id) as listings
 from 
   titles 
 
+--find gift in title inflation yoy
+  select 
+  '2019' as year
+  , case when regexp_contains(b.title, "(\?i)\\bgift|\\bcadeau|\\bregalo|\\bgeschenk|\\bprezent|ギフト") then 1 else 0 end as gift_title
+  , count(distinct a.listing_id) as active_listings
+from 
+  etsy-data-warehouse-prod.incrementals.listing_daily a
+inner join 
+  etsy-data-warehouse-prod.listing_mart.listing_titles b
+    using(listing_id)
+where 
+  (a.date between '2019-01-01' and '2019-04-09')
+group by 1,2
+union all 
+select 
+'2020' as year  
+  , case when regexp_contains(b.title, "(\?i)\\bgift|\\bcadeau|\\bregalo|\\bgeschenk|\\bprezent|ギフト") then 1 else 0 end as gift_title
+  , count(distinct a.listing_id) as active_listings
+from 
+  etsy-data-warehouse-prod.incrementals.listing_daily a
+inner join 
+  etsy-data-warehouse-prod.listing_mart.listing_titles b
+    using(listing_id)
+where 
+  (a.date between '2020-01-01' and '2020-04-09')
+group by 1,2
+union all 
+select 
+'2021' as year
+  , case when regexp_contains(b.title, "(\?i)\\bgift|\\bcadeau|\\bregalo|\\bgeschenk|\\bprezent|ギフト") then 1 else 0 end as gift_title
+  , count(distinct a.listing_id) as active_listings
+from 
+  etsy-data-warehouse-prod.incrementals.listing_daily a
+inner join 
+  etsy-data-warehouse-prod.listing_mart.listing_titles b
+    using(listing_id)
+where 
+  (a.date between '2021-01-01' and '2021-04-09')
+group by 1,2
+union all 
+select 
+'2022' as year
+  , case when regexp_contains(b.title, "(\?i)\\bgift|\\bcadeau|\\bregalo|\\bgeschenk|\\bprezent|ギフト") then 1 else 0 end as gift_title
+  , count(distinct a.listing_id) as active_listings
+from 
+  etsy-data-warehouse-prod.incrementals.listing_daily a
+inner join 
+  etsy-data-warehouse-prod.listing_mart.listing_titles b
+    using(listing_id)
+where 
+  (a.date between '2022-01-01' and '2022-04-09')
+group by 1,2
+union all 
+select 
+'2023' as year
+, case when regexp_contains(b.title, "(\?i)\\bgift|\\bcadeau|\\bregalo|\\bgeschenk|\\bprezent|ギフト") then 1 else 0 end as gift_title
+  , count(distinct a.listing_id) as active_listings
+from 
+  etsy-data-warehouse-prod.incrementals.listing_daily a
+inner join 
+  etsy-data-warehouse-prod.listing_mart.listing_titles b
+    using(listing_id)
+where 
+  (a.date between '2023-01-01' and '2023-04-09')
+group by 1,2
+union all 
+select 
+'2024' as year
+  , case when regexp_contains(b.title, "(\?i)\\bgift|\\bcadeau|\\bregalo|\\bgeschenk|\\bprezent|ギフト") then 1 else 0 end as gift_title
+  , count(distinct a.listing_id) as active_listings
+from 
+  etsy-data-warehouse-prod.incrementals.listing_daily a
+inner join 
+  etsy-data-warehouse-prod.listing_mart.listing_titles b
+    using(listing_id)
+where 
+  (a.date between '2024-01-01' and '2024-04-09')
+group by 1,2
 ----------------------------------------------------
 GIFT SEARCHING BEHAVIOR 
 ----------------------------------------------------
