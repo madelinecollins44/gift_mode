55911c5f0a08cdbe725b83af7274246106e3d869
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-04-16 10:22:00 -0400

    Update understanding_gms_growth.sql

--- understanding_gms_growth.sql
+++ understanding_gms_growth.sql
@@ -145,6 +145,67 @@ inner join
 where 
   (a.date between '2024-01-01' and '2024-04-09')
 group by 1,2
+
+--find transactions w gift in listing title yoy
+select 
+  '2019' as year
+  , count(distinct case when regexp_contains(b.title, "(\?i)\\bgift|\\bcadeau|\\bregalo|\\bgeschenk|\\bprezent|ギフト") then a.transaction_id end) as gift_transactions
+  , count(distinct a.transaction_id) as transactions
+from 
+  (select listing_id, transaction_id from etsy-data-warehouse-prod.transaction_mart.all_transactions a where a.date between '2019-01-01' and '2019-04-09') a
+inner join 
+  etsy-data-warehouse-prod.listing_mart.listing_titles b
+    using(listing_id)
+UNION ALL
+ select 
+  '2020' as year
+  , count(distinct case when regexp_contains(b.title, "(\?i)\\bgift|\\bcadeau|\\bregalo|\\bgeschenk|\\bprezent|ギフト") then a.transaction_id end) as gift_transactions
+  , count(distinct a.transaction_id) as transactions
+from 
+  (select listing_id, transaction_id from etsy-data-warehouse-prod.transaction_mart.all_transactions a where a.date between '2020-01-01' and '2020-04-09') a
+inner join 
+  etsy-data-warehouse-prod.listing_mart.listing_titles b
+    using(listing_id)
+UNION ALL
+ select 
+  '2021' as year
+  , count(distinct case when regexp_contains(b.title, "(\?i)\\bgift|\\bcadeau|\\bregalo|\\bgeschenk|\\bprezent|ギフト") then a.transaction_id end) as gift_transactions
+  , count(distinct a.transaction_id) as transactions
+from 
+  (select listing_id, transaction_id from etsy-data-warehouse-prod.transaction_mart.all_transactions a where a.date between '2021-01-01' and '2021-04-09') a
+inner join 
+  etsy-data-warehouse-prod.listing_mart.listing_titles b
+    using(listing_id)
+UNION ALL
+ select 
+  '2022' as year
+  , count(distinct case when regexp_contains(b.title, "(\?i)\\bgift|\\bcadeau|\\bregalo|\\bgeschenk|\\bprezent|ギフト") then a.transaction_id end) as gift_transactions
+  , count(distinct a.transaction_id) as transactions
+from 
+  (select listing_id, transaction_id from etsy-data-warehouse-prod.transaction_mart.all_transactions a where a.date between '2022-01-01' and '2022-04-09') a
+inner join 
+  etsy-data-warehouse-prod.listing_mart.listing_titles b
+    using(listing_id)
+UNION ALL
+ select 
+  '2023' as year
+  , count(distinct case when regexp_contains(b.title, "(\?i)\\bgift|\\bcadeau|\\bregalo|\\bgeschenk|\\bprezent|ギフト") then a.transaction_id end) as gift_transactions
+  , count(distinct a.transaction_id) as transactions
+from 
+  (select listing_id, transaction_id from etsy-data-warehouse-prod.transaction_mart.all_transactions a where a.date between '2023-01-01' and '2023-04-09') a
+inner join 
+  etsy-data-warehouse-prod.listing_mart.listing_titles b
+    using(listing_id)
+UNION ALL
+ select 
+  '2024' as year
+  , count(distinct case when regexp_contains(b.title, "(\?i)\\bgift|\\bcadeau|\\bregalo|\\bgeschenk|\\bprezent|ギフト") then a.transaction_id end) as gift_transactions
+  , count(distinct a.transaction_id) as transactions
+from 
+  (select listing_id, transaction_id from etsy-data-warehouse-prod.transaction_mart.all_transactions a where a.date between '2024-01-01' and '2024-04-09') a
+inner join 
+  etsy-data-warehouse-prod.listing_mart.listing_titles b
+    using(listing_id)
 ----------------------------------------------------
 GIFT SEARCHING BEHAVIOR 
 ----------------------------------------------------
