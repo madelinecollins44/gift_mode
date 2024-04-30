8efee5fbd6dc11a5717c1ca0fcc167a3cf03b18b
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-04-30 11:18:12 -0400

    Update gift_title_query_gms_opp_sizing.sql

--- gift_title_query_gms_opp_sizing.sql
+++ gift_title_query_gms_opp_sizing.sql
@@ -1,3 +1,56 @@
+---------------------------------------------
+TOP QUERIES IN TIAG ORDERS
+--------------------------------------------
+with tiag_orders as (
+select 
+  a.is_gift
+  , b.visit_id
+  , sum(c.trans_gms_net) as trans_gms_net
+from 
+  etsy-data-warehouse-prod.transaction_mart.all_transactions a
+inner join 
+  etsy-data-warehouse-prod.transaction_mart.transactions_visits b
+  using (transaction_id)
+left join 
+  etsy-data-warehouse-prod.transaction_mart.transactions_gms_by_trans c
+    on a.transaction_id=c.transaction_id
+where 
+  a.date >= current_date-30
+  and a.is_gift=1
+group by all
+)
+select 
+query 
+, count(distinct a.visit_id) as unique_visits
+, count(a.visit_id) as total_searches
+from 
+  tiag_orders a
+inner join 
+  etsy-data-warehouse-prod.search.query_sessions_new b
+    using (visit_id)
+where b._date>= current_date-30
+group by all
+order by 3 desc
+
+	
+------------------
+QUERIES BY GIFTINESS SCORE
+----------------
+select
+  count(distinct visit_id) as unique_visits
+  , count(visit_id) as searches
+  , count(distinct query) as unique_queries 
+from 
+  etsy-data-warehouse-prod.knowledge_base.query_giftiness a
+inner join 
+  etsy-data-warehouse-prod.search.query_sessions_new b using (query)
+where 
+  a._date >= current_date-30 
+  and b._date >= current_date-30 
+group by all 
+having avg(overall_giftiness) >= 0.61
+order by 2 desc
+	
 ------------------
 GIFT QUERY
 ----------------
