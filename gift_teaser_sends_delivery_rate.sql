8121ad75b2b97b5ab628366fb381d498f5cd9a47
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-05-21 14:24:06 -0400

    Update gift_teaser_sends_delivery_rate.sql

--- gift_teaser_sends_delivery_rate.sql
+++ gift_teaser_sends_delivery_rate.sql
@@ -116,3 +116,62 @@ group by all
 );
 
 END
+
+
+------------------------------------------------------------
+--CHECK TO SEE IF RECEIPT_IDS HAVE MULTIPLE CAMPAIGNS
+------------------------------------------------------------
+-- with gift_teasers as (
+-- select
+-- date(timestamp_seconds(create_date)) as create_date
+-- , receipt_id
+-- , create_page_source
+-- , email_send_schedule_option
+-- , date(timestamp_seconds(email_sent_date)) as email_sent_date
+-- , lower(recipient_email) as recipient_email
+-- from
+-- `etsy-data-warehouse-prod.etsy_shard.gift_receipt_options`
+-- where
+-- date(timestamp_seconds(create_date)) >= current_date-90 --all gift teasers created in 90 days
+-- and date(timestamp_seconds(create_date)) < current_date -- create before current_date
+-- and email_send_schedule_option != 2
+-- and gifting_token is not null
+-- and email_sent_date is not null -- has been sent
+-- and delete_date is null
+-- and recipient_email > ""
+-- and (date(timestamp_seconds(email_scheduled_send_date))) > current_date-30
+-- )
+-- , find_campaigns as (
+-- select
+-- create_date
+-- , email_sent_date
+-- , a.receipt_id
+-- , b.campaign_label
+-- , a.recipient_email
+-- , a.email_send_schedule_option
+-- , count(case when b.campaign_label like "recipient_%" then b.campaign_label end) as recipient_campaigns
+-- , count(case when b.campaign_label = "recipient_gift_receipt_buyercopy_2024_email" then b.campaign_label end) as recipient_gift_receipt_buyercopy_2024_email
+-- , count(case when b.campaign_label = "recipient_gift_receipt_specific_date" then b.campaign_label end) as recipient_gift_receipt_specific_date
+-- , count(case when b.campaign_label = "recipient_gift_receipt_afterpurchase" then b.campaign_label end) as recipient_gift_receipt_afterpurchase
+-- , count(case when b.campaign_label = "recipient_gift_receipt_aftershipped" then b.campaign_label end) as recipient_gift_receipt_aftershipped
+-- from
+-- gift_teasers a
+-- left join
+-- `etsy-data-warehouse-prod.mail_mart.delivered` b
+-- on
+-- a.recipient_email = lower(b.email_address)
+-- and b.campaign_label like "recipient_%"
+-- and date(timestamp_seconds(b.send_date)) = a.email_sent_date
+-- and date(timestamp_seconds(b.send_date)) >= current_date-30
+-- group by all
+-- order by a.receipt_id desc
+-- )
+-- select
+-- *
+-- from
+-- find_campaigns a
+-- where
+-- (CASE WHEN recipient_gift_receipt_specific_date > 0 THEN 1 ELSE 0 END +
+-- CASE WHEN recipient_gift_receipt_afterpurchase > 0 THEN 1 ELSE 0 END +
+-- CASE WHEN recipient_gift_receipt_aftershipped > 0 THEN 1 ELSE 0 END) >= 2
+-- group by all
