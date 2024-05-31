ea257a982e378024922ffc3df06594a6067e855d
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-05-31 16:46:13 -0400

    Update gift_mode_framework.sql

--- gift_mode_framework.sql
+++ gift_mode_framework.sql
@@ -26,7 +26,9 @@ values
   , ('gift_mode_introduction_modal_shown') --Gift Mode introduction overlay shown on homescreen, boe
 ;
 
-
+------------------------------------
+--VISITS AND IMPRESSIONS
+------------------------------------
 --this table grabs visits across gift mode related content and core gift mode pages 
 create or replace temporary table visits as (
   with get_recmods_events as (
@@ -81,8 +83,10 @@ where
 group by all 
 );
 
-------------clicks + reftags
-		select 
+------------------------------------
+--CLICKS
+------------------------------------		
+select 
 	_date 
 	, visit_id 
 	, sequence_number 
@@ -104,5 +108,92 @@ ref_tag in
 			like ('listing_suggested_persona_listings_related%') --Related personas module on listing page, web
 			or like ('gm%') -- general ref_tags, web
     )
-    
+
+------------------------------------
+--LISTING VIEWS 
+------------------------------------
+create or replace temporary table listing_gms as (
+select
+	tv.date as _date
+	, tv.visit_id
+	, tv.platform_app as platform
+	, tv.transaction_id
+	, t.listing_id
+	, tg.trans_gms_net
+from
+	`etsy-data-warehouse-prod`.transaction_mart.transactions_visits tv
+join
+	`etsy-data-warehouse-prod`.transaction_mart.transactions_gms_by_trans tg
+using(transaction_id)
+join
+	`etsy-data-warehouse-prod`.transaction_mart.all_transactions t
+on
+	tv.transaction_id = t.transaction_id
+where
+	tv.date >= last_date
+)
+;
+create or temporary table listing_views as (
+with listing_views as (
+select 
+	_date 
+	, visit_id 
+	, sequence_number 
+	, safe_cast(listing_id as int64) as listing_id
+	, regexp_substr(e.referrer, "ref=([^*&?%|]+)") as boe_ref 
+	, ref_tag
+from 
+	`etsy-data-warehouse-prod`.weblog.events e 
+where 
+	_date >= last_date
+	and event_type = "view_listing"
+)
+, agg as (
+select
+	b._date  
+	, b.platform 
+  , b.browser_platform 
+	, b.region  
+  , b.is_admin_visit as admin 
+  , b.top_channel 
+  , a.listing_id
+  , a.visit_id
+  , coalesce(count(*),0) as n_listing_views
+	, coalesce(max(c.purchased_after_view),0) as purchased_after_view
+from 
+  listing_views a
+inner join 
+  etsy-data-warehouse-prod.weblog.visits b
+    using (_date, visit_id)
+left join 
+  `etsy-data-warehouse-prod`.analytics.listing_views c
+    on a.listing_id=c.listing_id
+    and a.visit_id=c.visit_id
+    and a._date=c._date
+    and a.sequence_number=a.sequence_number 
+where 
+  ref_tag like ('gm_gift_idea_listings%') -- persona listing view, web
+  or ref_tag like ('gm_occasion_gift_idea_listings-%') -- occasion listing view, web
+  or ref_tag like ('gm_deluxe_persona_card%') -- quiz listing view, web
+  or boe_referrer like ('boe_gift_mode_popular_gift_listings%') -- popular gift ideas listing view, web
+  or boe_referrer like ('boe_gift_mode_gift_idea_listings%')-- persona listing view, boe
+  or boe_referrer like ('gift_mode_occasion%')-- occasion listing view, boe
+  or boe_referrer like ('boe_gift_mode_editors_picks_listings%')-- occasion listing view, boe NEED TO CONFIRM
+)
+select
+	a._date
+	, a.platform
+	, a.region
+	, a.is_admin_visit
+	, a.top_channel
+	, a.persona_id
+	, sum(a.n_listing_views) as total_listing_views
+	, coalesce(count(case when (a.ref_tag like ('boe_gift_mode_gift_idea_listings%') or a.ref_tag like ('gm_gift_idea_listings%')) then a.visit_id end),0) as total_persona_page_listing_views
+	, count(distinct a.listing_id) as unique_listings_viewed
+	, count(distinct transaction_id) as unique_transactions
+	, sum(a.purchased_after_view) as total_purchased_listings
+	, coalesce(sum(b.trans_gms_net),0) as attr_gms
+from agg a
+left join listing_gms b
+);
 
