0f31274bf7534d15b058c395d2f783a308ce434a
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-06-03 15:58:27 -0400

    Update gift_mode_framework.sql

--- gift_mode_framework.sql
+++ gift_mode_framework.sql
@@ -1,35 +1,9 @@
 ---can i make a temp table with all necessary events and update there instead of inquery as more events are added?
 begin
 
---create table with all core gift mode events
-create or replace temporary table core_pages (event_name STRING);
-insert into core_pages (event_name)
-values
-('gift_mode_home') --gift mode home, boe + web
-  , ('gift_mode_persona')-- gift mode personas, boe + web 
-  , ('gift_mode_occasions_page')-- gift mode occasions, web
-  , ('gift_mode_browse_all_personas') -- see all personas, web
-  , ('gift_mode_see_all_personas') -- see all personas, boe
-  , ('gift_mode_results') -- gift mode quiz results, web
-  , ('gift_mode_quiz_results') -- gift mode quiz results, boe
-;
-
---create table with all core gift mode events
-create or replace temporary table other_content (event_name STRING);
-insert into other_content (event_name)
-values
-  ('gift_mode_shop_by_occasions_module_seen') --shop by occasion module on homepage, web
-  , ('gm_gift_page_ingress_loaded') -- gift mode promo banner on gift category page, web
-  , ('search_gift_mode_banner_seen') -- gift mode promo banner on search page, web
-  , ('gm_hp_banner_loaded_seen') --homepage banner, web
-  , ('search_gift_mode_banner_seen')-- bottom of search page for gift queries, web
-  , ('gift_mode_introduction_modal_shown') --Gift Mode introduction overlay shown on homescreen, boe
-;
-
-------------------------------------
---VISITS AND IMPRESSIONS
-------------------------------------
---this table grabs visits across gift mode related content and core gift mode pages 
+------------------------------------------------------------------------------------------------------------
+--VISITS AND IMPRESSIONS: this table grabs visits across gift mode related content and core gift mode pages 
+------------------------------------------------------------------------------------------------------------
 create or replace temporary table visits as (
   with get_recmods_events as (
   select
@@ -43,7 +17,7 @@ create or replace temporary table visits as (
 	where 
     date(_partitiontime) >= current_date-2
 	  and ((beacon.event_name = 'recommendations_module_delivered' 
-        and ((select value from unnest(beacon.properties.key_value) where key = 'module_placement') in ('lp_suggested_personas_related','homescreen_gift_mode_personas') --related personas module on listing page, web AND app home popular personas module delivered, boe
+        and ((select value from unnest(beacon.properties.key_value) where key = 'module_placement') in ('lp_suggested_personas_related','homescreen_gift_mode_personas')) --related personas module on listing page, web AND app home popular personas module delivered, boe
         or (select value from unnest(beacon.properties.key_value) where key = 'module_placement') like ('hub_stashgrid_module-%') --Featured personas on hub, web
     or beacon.event_name in 
     ------various ingresses + banners 
@@ -81,39 +55,36 @@ inner join
 where 
   a._date >= current_date-2
 group by all 
+)
 );
 
 ------------------------------------
 --CLICKS
 ------------------------------------		
+create or replace temporary table clicks as (
+with get_refs as (
 select 
 	_date 
 	, visit_id 
 	, sequence_number 
-  -- , 
 	-- , regexp_substr(e.referrer, "ref=([^*&?%|]+)") as boe_ref 
 	, ref_tag
 from 
 	`etsy-data-warehouse-prod`.weblog.events e 
 where 
-	_date >= current_date-3
-ref_tag in
-	('hp_gm_shop_by_occasion_module' -- Shop by occasion on homepage, web
-	, 'listing_suggested_personas_related' --Related personas module/ personas variant, web
-	, 'hub_GiftMode' --Gift Teaser promo banner on hub, web
-	, 'hp_promo_secondary_042224_US_Gifts_%' -- Onsite Promo Banner (Mother's Day/ Father's Day), web
-	, 'hp_promo_tertiary_042224_US_Gifts_%' -- Onsite Promo Banner (Mother's Day/ Father's Day), web
-	, 'GiftTeaser_MDAY24_Skinny_Sitewide' -- Skinny Banner (Mother's Day), web
-	and (ref_tag 
-			like ('listing_suggested_persona_listings_related%') --Related personas module on listing page, web
-			or like ('gm%') -- general ref_tags, web
-    )
+	_date >= last_date
+  and (ref_tag in ('hp_gm_shop_by_occasion_module' -- Shop by occasion on homepage, web
+	    , 'listing_suggested_personas_related' --Related personas module/ personas variant, web
+	    , 'hub_GiftMode' --Gift Teaser promo banner on hub, web
+      , 'GiftTeaser_MDAY24_Skinny_Sitewide')) -- Skinny Banner (Mother's Day), web
+	or (ref_tag like ('hp_promo_secondary_042224_US_Gifts_%')-- Onsite Promo Banner (Mother's Day/ Father's Day), web
+      or ref_tag like ('hp_promo_tertiary_042224_US_Gifts_%'))-- Onsite Promo Banner (Mother's Day/ Father's Day), web
+)
+--how do i want to agg these? do i want to break it down by core vs all gift mode visits? 
 
 ------------------------------------
 --LISTING VIEWS 
 ------------------------------------
-begin 
-
 create or replace temporary table listing_gms as (
 select
 	tv.date as _date
@@ -176,12 +147,14 @@ left join
     and a.sequence_number=a.sequence_number 
 where 
   b._date >= current_date-2
-  and (ref_tag like ('gm_%') or ref_tag like ('gift_mode_%'))
+  and (ref_tag like ('gm_%') or ref_tag like ('gift_mode_%')) -- find ref tags of non-core visits 
   or boe_referrer like ('boe_gift_mode%') 
+
 	
   -- and (ref_tag like ('gm_gift_idea_listings%') -- persona listing view, web
   -- or ref_tag like ('gm_occasion_gift_idea_listings-%') -- occasion listing view, web
   -- or ref_tag like ('gm_deluxe_persona_card%') -- quiz listing view, web
+  -- or ref_tag like ('listing_suggested_persona_listings_related%') -- listings at bottom of listing page, web, NOT CORE 
   -- or boe_referrer like ('boe_gift_mode_popular_gift_listings%') -- popular gift ideas listing view, web
   -- or boe_referrer like ('boe_gift_mode_gift_idea_listings%')-- persona listing view, boe
   -- or boe_referrer like ('gift_mode_occasion%')-- occasion listing view, boe
