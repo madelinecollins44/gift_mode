c5bf24d5f737af6408b68d6e3d0323f86858952a
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-06-03 16:11:01 -0400

    Update gift_mode_framework.sql'

--- gift_mode_framework.sql
+++ gift_mode_framework.sql
@@ -19,22 +19,23 @@ create or replace temporary table visits as (
 	  and ((beacon.event_name = 'recommendations_module_delivered' 
         and ((select value from unnest(beacon.properties.key_value) where key = 'module_placement') in ('lp_suggested_personas_related','homescreen_gift_mode_personas')) --related personas module on listing page, web AND app home popular personas module delivered, boe
         or (select value from unnest(beacon.properties.key_value) where key = 'module_placement') like ('hub_stashgrid_module-%') --Featured personas on hub, web
-    or beacon.event_name in 
+            or (select value from unnest(beacon.properties.key_value) where key = 'module_placement') like ('hub_stashgrid_module-%') --Featured personas on hub, web
+    or (beacon.event_name like ('%gm_%') or beacon.event_name like ('%gift_mode%')
     ------various ingresses + banners 
-    ('gift_mode_shop_by_occasions_module_seen' --shop by occasion module on homepage, web
-    , 'gm_gift_page_ingress_loaded' -- gift mode promo banner on gift category page, web
-    , 'search_gift_mode_banner_seen' -- gift mode promo banner on search page, web
-    , 'gm_hp_banner_loaded_seen' --homepage banner, web
-    , 'search_gift_mode_banner_seen'-- bottom of search page for gift queries, web
-    , 'gift_mode_introduction_modal_shown' --Gift Mode introduction overlay shown on homescreen, boe
-     ------core visits 
-    , 'gift_mode_home' --gift mode home, boe + web
-    , 'gift_mode_persona'-- gift mode personas, boe + web 
-    , 'gift_mode_occasions_page'-- gift mode occasions, web
-    , 'gift_mode_browse_all_personas' -- see all personas, web
-    , 'gift_mode_see_all_personas' -- see all personas, boe
-    , 'gift_mode_results' -- gift mode quiz results, web
-    , 'gift_mode_quiz_results'))-- gift mode quiz results, boe
+    -- ('gift_mode_shop_by_occasions_module_seen' --shop by occasion module on homepage, web
+    -- , 'gm_gift_page_ingress_loaded' -- gift mode promo banner on gift category page, web
+    -- , 'search_gift_mode_banner_seen' -- gift mode promo banner on search page, web
+    -- , 'gm_hp_banner_loaded_seen' --homepage banner, web
+    -- , 'search_gift_mode_banner_seen'-- bottom of search page for gift queries, web
+    -- , 'gift_mode_introduction_modal_shown' --Gift Mode introduction overlay shown on homescreen, boe
+    --  ------core visits 
+    -- , 'gift_mode_home' --gift mode home, boe + web
+    -- , 'gift_mode_persona'-- gift mode personas, boe + web 
+    -- , 'gift_mode_occasions_page'-- gift mode occasions, web
+    -- , 'gift_mode_browse_all_personas' -- see all personas, web
+    -- , 'gift_mode_see_all_personas' -- see all personas, boe
+    -- , 'gift_mode_results' -- gift mode quiz results, web
+    -- , 'gift_mode_quiz_results'))-- gift mode quiz results, boe
 )
 select 
 	b._date  
