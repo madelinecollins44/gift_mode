681c8c2cb654ed029ca4ef297b4ccab2060fcc8f
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-06-04 11:01:57 -0400

    Update editorial_content_in_gm.sql

--- editorial_content_in_gm.sql
+++ editorial_content_in_gm.sql
@@ -1,10 +1,6 @@
 -------------------------------------------------------------------------
 --CLICK RATE OF EDITORIAL CONTENT ON GIFT MODE PAGES AT THE MODULE LEVEL
---how doep modules perform against non-ep modules?
--------------------------------------------------------------------------
--------------------------------------------------------------------------
---CLICK RATE OF EDITORIAL CONTENT ON GIFT MODE PAGES AT THE MODULE LEVEL
---how doep modules perform against non-ep modules?
+--how does modules perform against non-ep modules?
 -------------------------------------------------------------------------
 with impressions as (
 select
@@ -14,11 +10,15 @@ select
 	, beacon.event_name as event_name
 	-- , (select value from unnest(beacon.properties.key_value) where key = 'listing_ids') as listing_ids
 from
-	`etsy-visit-pipe-prod.canonical.visit_id_beacons`
+	`etsy-visit-pipe-prod.canonical.visit_id_beacons` a
+inner join  
+  etsy-data-warehouse-prod.weblog.visits b
+    using (visit_id)
 where
-	date(_partitiontime) >= current_date-2
-and
-	beacon.event_name in ('gift_mode_persona', 'gift_mode_occasions_page') -- using primary pages bc ep delivery event on occasions page does not exist 
+	date(_partitiontime) >= current_date-30
+  and beacon.event_name in ('gift_mode_persona', 'gift_mode_occasions_page','popular_gift_listings_delivered') -- using primary pages bc ep delivery event on occasions page does not exist
+  and b._date >= current_date-30
+  and b.platform in ('mobile_web','desktop') 
 )
 , clicks as (
 select
@@ -32,21 +32,25 @@ select
 	from
 		`etsy-visit-pipe-prod`.canonical.visit_id_beacons
 	where 
-    date(_partitiontime) >= current_date-2
+    date(_partitiontime) >= current_date-30
 	  and beacon.event_name in ('view_listing')
     and (regexp_substr(beacon.loc, 'ref=([^*&?%]+)') like ('gm_editorial_listings%') -- persona page ep clicks
-        or regexp_substr(beacon.loc, 'ref=([^*&?%]+)') like ('gm_occasions_etsys_picks%')) -- occasion page ep clicks 
+        or regexp_substr(beacon.loc, 'ref=([^*&?%]+)') like ('gm_occasions_etsys_picks%')
+        or regexp_substr(beacon.loc, 'ref=([^*&?%]+)') like ('gm_popular_gift_listings%'))
 )
 select
-  count(case when b.ref_tag_clean in ('gm_editorial_listings') then b.visit_id end) as ep_persona_clicks
-  , count(case when a.event_name in ('gift_mode_persona') then a.visit_id end) as ep_persona_impressions
-  , count(case when b.ref_tag_clean in ('gift_occasion_etsys_picks_delivered') then b.visit_id end)  as ep_occasion_clicks
+  count(case when a.event_name in ('gift_mode_persona') then a.visit_id end) as ep_persona_impressions
+  , count(case when b.ref_tag_clean in ('gm_editorial_listings') then b.visit_id end) as ep_persona_clicks
   , count(case when a.event_name in ('gift_mode_occasions_page') then a.visit_id end) as ep_occasion_impressions
+  , count(case when b.ref_tag_clean in ('gm_occasions_etsys_picks') then b.visit_id end)  as ep_occasion_clicks
+  , count(case when a.event_name in ('popular_gift_listings_delivered') then a.visit_id end) as ep_home_impressions
+  , count(case when b.ref_tag_clean in ('gm_popular_gift_listings') then b.visit_id end)  as ep_home_clicks
 from 
   impressions a
 left join 
   clicks b
     using (_date, visit_id)
+
 --------------------------------------------------------------------------------------------------------------------------------------------------
 --CLICK RATE OF STASH LISTINGS VS OTHER LISTINGS
 -- looking at the listings delivered in gift ideas, do stash listings perform better than other listings?
