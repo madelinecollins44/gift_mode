3a9e584ed1c1ec92ad8589067a2a19a4769dffb4
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-06-04 11:23:12 -0400

    Update editorial_content_in_gm.sql

--- editorial_content_in_gm.sql
+++ editorial_content_in_gm.sql
@@ -62,4 +62,72 @@ left join
 --CLICK RATE OF STASH LISTINGS VS OTHER LISTINGS
 -- looking at the listings delivered in gift ideas, do stash listings perform better than other listings?
 --------------------------------------------------------------------------------------------------------------------------------------------------
+with get_stash_listings as (
+select 
+  distinct l.listing_id,
+from 
+  `etsy-data-warehouse-prod`.listing_mart.listings as l
+left outer join 
+  `etsy-data-warehouse-prod`.etsy_shard.merch_listings m
+    using (listing_id)
+where  
+  m.status = 0
+  and l.is_active = 1
+), all_gift_idea_deliveries as ( -- get all listings delivered in gift idea modules 
+	select
+		date(_partitiontime) as _date
+		, visit_id
+		, sequence_number
+		, beacon.event_name as event_name
+		, (select value from unnest(beacon.properties.key_value) where key = "module_placement") as module_placement
+    , split((select value from unnest(beacon.properties.key_value) where key = "module_placement"), "-")[safe_offset(0)] as module_placement_clean
+    , (select value from unnest(beacon.properties.key_value) where key = "listing_ids") as listing_ids
+	from
+		`etsy-visit-pipe-prod.canonical.visit_id_beacons`
+	where date(_partitiontime) >= current_date-3
+	  and beacon.event_name = "recommendations_module_delivered"
+	  and ((select value from unnest(beacon.properties.key_value) where key = "module_placement") like ("gift_mode_occasion_gift_idea_%") -- mweb/ desktop occasions
+        or (select value from unnest(beacon.properties.key_value) where key = "module_placement") like ("gift_mode_gift_idea_listings%") -- mweb/ desktop personas
+))
+, clean_deliveries as ( -- clean up gift idea listing delivery
+select 
+  a._date 
+  , a.visit_id
+  , listing_id
+from 
+  all_gift_idea_deliveries a
+cross join 
+   unnest(split(listing_ids, ',')) as listing_id
+)
+, ref_tags as ( -- get listings that have a gift_idea related ref tag
+select
+  date(a._partitiontime) as _date
+  , a.visit_id
+  , a.sequence_number
+  , beacon.event_name as event_name
+	, regexp_substr(beacon.loc, 'ref=([^*&?%]+)') as ref_tag
+  , split(regexp_substr(beacon.loc, 'ref=([^*&?%]+)'), "-")[safe_offset(0)] as ref_tag_clean    
+  , (select value from unnest(beacon.properties.key_value) where key = "listing_id") as listing_id
+from 
+  `etsy-visit-pipe-prod.canonical.visit_id_beacons`a
+where 
+  beacon.event_name in ('view_listing')
+  and (regexp_substr(beacon.loc, 'ref=([^*&?%]+)') like ('gm_occasion_gift_idea_listings%')
+       or regexp_substr(beacon.loc, 'ref=([^*&?%]+)') like ('gm_gift_idea_listings%')) -- comes from gift mode 
+  and date(_partitiontime) >= current_date-3
+)
+select 
+	count(a.listing_id) as listing_impression
+	, count(b.listing_id) as listing_click
+	, count(case when c.listing_id is not null then a.listing_id end) as stash_listing_impression
+	, count(case when c.listing_id is not null then b.listing_id end) as stash_listing_click
+from 
+	clean_deliveries a
+left join 
+	ref_tags b
+		using (visit_id, listing_id, _date)
+left join 
+	get_stash_listings c 
+		on a.listing_id=cast(c.listing_id as string)
+
 
