bbaf3057781f9bccebd9c0f5c6f2029f15c541f4
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-07-10 12:49:28 -0400

    Update gift_ideas_troubleshooting.sql

--- gift_ideas_troubleshooting.sql
+++ gift_ideas_troubleshooting.sql
@@ -1,3 +1,5 @@
+
+******NARROWED IT DOWN TO ISSUE BEING IN FINAL JOIN -- test indivial gift idea to see where issue is makes sense so must be in join
 ________________________________________________________
 --check to make sure raw data from beacons table matches what is getting put out in rollup
 ________________________________________________________
@@ -85,3 +87,122 @@ select _date, sum(all_pageviews) as all_pageviews from etsy-data-warehouse-prod.
 group by all order by 1 desc
 
 ----the impressions are very different over time, more changes than persona pageview 
+
+
+________________________________________________________
+--test indivial gift idea to see where issue is
+________________________________________________________
+--what the number should be, from beacons table 
+with beacons_table_raw as (
+	select
+		date(_partitiontime) as _date
+		, visit_id
+		, sequence_number
+  , concat(visit_id, '-', sequence_number) AS unique_id
+		, beacon.event_name as event_name
+		, (select value from unnest(beacon.properties.key_value) where key = "module_placement") as module_placement
+    , split((select value from unnest(beacon.properties.key_value) where key = "module_placement"), "-")[safe_offset(0)] as module_placement_clean
+		, (select value from unnest(beacon.properties.key_value) where key = "gift_idea_id") as gift_idea_id 
+    , (select value from unnest(beacon.properties.key_value) where key = "listing_ids") as listing_ids
+    , (select value from unnest(beacon.properties.key_value) where key = "occasion_id") as occasion_id
+    , (select value from unnest(beacon.properties.key_value) where key = "persona_id") as persona_id
+	from
+		`etsy-visit-pipe-prod.canonical.visit_id_beacons`
+	where date(_partitiontime) >= current_date-10
+	  and (beacon.event_name = "recommendations_module_delivered")
+	  and ((select value from unnest(beacon.properties.key_value) where key = "module_placement") like ("gift_mode_occasion_gift_idea_listings%") -- mweb/ desktop occasions
+        or (select value from unnest(beacon.properties.key_value) where key = "module_placement") like ("gift_mode_gift_idea_listings%") -- mweb/ desktop personas
+        or (select value from unnest(beacon.properties.key_value) where key = "module_placement") like ("boe_gift_mode_gift_idea_listings%") -- boe personas
+        or (select value from unnest(beacon.properties.key_value) where key = "module_placement") like ("boe_gift_mode_search_listings%")) -- boe search 
+)
+-- , beacons_table as (
+select
+  gift_idea_id
+  , _date
+  , count(distinct concat(visit_id, '-', sequence_number)) AS unique_id
+    , case 
+      when module_placement_clean in ('gift_mode_occasion_gift_idea_listings') then 'gift_mode_occasions_page' 
+      when module_placement_clean in ('gift_mode_gift_idea_listings','boe_gift_mode_gift_idea_listings') then 'gift_mode_persona' 
+      when module_placement_clean in ('boe_gift_mode_search_listings') then 'gift_mode_search' 
+      else 'error'
+      end as delivery_page
+from beacons_table_raw
+where gift_idea_id in ('fb3dc615-a696-4605-957f-affcbf5256f9')
+and _date= '2024-07-07'
+group by all
+)
+select 
+--3338 impressions persona
+
+--what is coming from cte 
+with all_gift_idea_deliveries as (
+	select
+		date(_partitiontime) as _date
+		, visit_id
+		, sequence_number
+      , concat(visit_id, '-', sequence_number) AS unique_id
+		, beacon.event_name as event_name
+		, (select value from unnest(beacon.properties.key_value) where key = "module_placement") as module_placement
+    , split((select value from unnest(beacon.properties.key_value) where key = "module_placement"), "-")[safe_offset(0)] as module_placement_clean
+		, (select value from unnest(beacon.properties.key_value) where key = "gift_idea_id") as gift_idea_id 
+    , (select value from unnest(beacon.properties.key_value) where key = "listing_ids") as listing_ids
+    , (select value from unnest(beacon.properties.key_value) where key = "occasion_id") as occasion_id
+    , (select value from unnest(beacon.properties.key_value) where key = "persona_id") as persona_id
+	from
+		`etsy-visit-pipe-prod.canonical.visit_id_beacons`
+	where date(_partitiontime) >= current_date-10
+	  and (beacon.event_name = "recommendations_module_delivered")
+	  and ((select value from unnest(beacon.properties.key_value) where key = "module_placement") like ("gift_mode_occasion_gift_idea_listings%") -- mweb/ desktop occasions
+        or (select value from unnest(beacon.properties.key_value) where key = "module_placement") like ("gift_mode_gift_idea_listings%") -- mweb/ desktop personas
+        or (select value from unnest(beacon.properties.key_value) where key = "module_placement") like ("boe_gift_mode_gift_idea_listings%") -- boe personas
+        or (select value from unnest(beacon.properties.key_value) where key = "module_placement") like ("boe_gift_mode_search_listings%")) -- boe search 
+)
+, deliveries as (
+select 
+  a._date 
+  , case 
+      when module_placement_clean in ('gift_mode_occasion_gift_idea_listings') then 'gift_mode_occasions_page' 
+      when module_placement_clean in ('gift_mode_gift_idea_listings','boe_gift_mode_gift_idea_listings') then 'gift_mode_persona' 
+      when module_placement_clean in ('boe_gift_mode_search_listings') then 'gift_mode_search' 
+      else 'error'
+    end as page_type
+  , coalesce(a.occasion_id, a.persona_id) as page_id
+  , a.gift_idea_id
+  , a.visit_id
+  , a.sequence_number
+  , concat(a.visit_id, '-', a.sequence_number) AS unique_id
+  , a.module_placement_clean
+  , listing_id
+from 
+  all_gift_idea_deliveries a
+cross join 
+   unnest(split(listing_ids, ',')) as listing_id
+group by all
+) 
+, agg as (select
+	b._date
+	, v.platform
+  , v.browser_platform
+	, v.region
+	, v.top_channel
+	, v.is_admin_visit as admin
+  , b.gift_idea_id
+	, b.page_type
+  , b.page_id
+  , count(distinct b.listing_id) as unique_listings
+  , count(distinct b.visit_id) as unique_visits
+	, count(distinct b.unique_id) as total_impressions -- this is each visits specific delivery of gift ideas
+  , count(b.listing_id) as total_listings_delivered -- will be used for listing rate
+from 
+	etsy-data-warehouse-prod.weblog.visits v
+left join 
+	deliveries b
+    using(_date, visit_id)
+where
+	v._date >= current_date-10
+group by all
+)
+select page_type, sum(total_impressions) from agg where gift_idea_id in ('fb3dc615-a696-4605-957f-affcbf5256f9')
+and _date= '2024-07-07' group by all
+--3338 impressions persona
+-------THESE MATCH--ISSUE MUST BE IN JOIN 
