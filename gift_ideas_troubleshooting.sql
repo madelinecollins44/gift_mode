e83c27fcf9f8d78d4f573be1252f0cf13b833305
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-07-10 10:56:31 -0400

    Update gift_ideas_troubleshooting.sql

--- gift_ideas_troubleshooting.sql
+++ gift_ideas_troubleshooting.sql
@@ -7,6 +7,7 @@ select
   , sum(total_impressions) as total_impressions
 from 
   etsy-data-warehouse-dev.rollups.gift_mode_gift_idea_stats
+	--  etsy-bigquery-adhoc-prod._script7065f0ce6d076ab97aea44909a86cf9dd0339f2a.rec_mod --> use this table instead to see where impressions are getting messed up 
 where _date >= current_date-2
 group by all
 )
@@ -50,8 +51,37 @@ group by all order by 3 desc
   
 ________________________________________________________
 --make sure each cte is unique on all dimensions 
+	--yes: all tables have 1 count
 ________________________________________________________
+select 
+	_date
+	, platform
+  , browser_platform
+	, region
+	, top_channel
+	, admin
+  , gift_idea_id
+	, page_type
+  , page_id
+  , count(*)
+from 
+  etsy-bigquery-adhoc-prod._script7065f0ce6d076ab97aea44909a86cf9dd0339f2a.rec_mod
+	--  etsy-bigquery-adhoc-prod._script7065f0ce6d076ab97aea44909a86cf9dd0339f2a.clicks
+group by all order by count(*) desc
+
+
+
+	etsy-bigquery-adhoc-prod._script7065f0ce6d076ab97aea44909a86cf9dd0339f2a.clicks
 
 ________________________________________________________
 --see if all days are higher or just certain days 
 ________________________________________________________
+--test gift idea impressions over time
+select _date, sum(total_impressions) as total_impressions from etsy-data-warehouse-prod.rollups.gift_mode_gift_idea_stats
+group by all order by 1 desc
+
+--test persona pageviews over time
+select _date, sum(all_pageviews) as all_pageviews from etsy-data-warehouse-prod.rollups.gift_mode_persona_stats
+group by all order by 1 desc
+
+----the impressions are very different over time, more changes than persona pageview 
