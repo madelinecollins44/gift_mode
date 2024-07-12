ce1f5a8e1f197638fb1acdb4591aef3c44790c56
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-07-12 12:25:02 -0400

    Create framework_rollup_testing.sql

---
+++ framework_rollup_testing.sql
@@ -0,0 +1,47 @@
+
+---------------------------------------------------------------
+--find visit_id and check against weblog.events, impressions
+---------------------------------------------------------------
+--impressions
+ith get_recmods_events as (
+  select
+		date(_partitiontime) as _date
+		, visit_id
+		, sequence_number
+    , beacon.primary_event as primary_event
+		, beacon.event_name as event_name
+		, (select value from unnest(beacon.properties.key_value) where key = 'module_placement') as module_placement
+	from
+		`etsy-visit-pipe-prod.canonical.visit_id_beacons` a
+	where 
+    date(_partitiontime) >= current_date-5
+    and --events
+      (beacon.event_name like ('%gm_%') or beacon.event_name like ('%gift_mode%') --catpures most gm content + core
+	      or (beacon.event_name = 'recommendations_module_delivered' -- rec mods from other outside gift mode 
+            and (select value from unnest(beacon.properties.key_value) where key = 'module_placement') in     ('lp_suggested_personas_related','homescreen_gift_mode_personas') --related personas module on listing page, web AND app home popular personas module delivered, boe
+            or (select value from unnest(beacon.properties.key_value) where key = 'module_placement') like ('market_gift_personas%')))--related/ popular persona module on market page, web
+)
+select 
+	b._date  
+	, a.platform 
+  , a.browser_platform 
+	, a.region  
+  , a.is_admin_visit as admin 
+  , a.top_channel 
+  , visit_id
+  , count(visit_id) as impressions
+  , max(case when event_name like ('%gift_mode%') and primary_event=true then 1 else 0 end) as core_visits
+  , count(case when event_name like ('%gift_mode%') and primary_event=true then visit_id end) as core_impressions
+from 
+  etsy-data-warehouse-prod.weblog.visits a
+inner join 
+  get_recmods_events b 
+    using (_date, visit_id)
+where 
+  a._date >= current_date-2
+group by all
+
+
+---------------------------------------------------------------
+--find visit_id and check against weblog.events, listing views
+---------------------------------------------------------------
